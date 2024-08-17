6f9882721deeaae09a4c7e4d8dc2f065fd7a5735
Kenny Yong <homeyong@gmail.com>
2024-08-17 22:36:38 +0800

    can transfer connect

--- src/layouts/sections/page-sections/myApp/components/FeatureOne.vue
+++ src/layouts/sections/page-sections/myApp/components/FeatureOne.vue
@@ -5,13 +5,18 @@ import { computed, ref } from 'vue';
 import { Buffer } from 'buffer'; // Import the buffer package
 
 // Import the necessary functions and libraries
-import { Connection, clusterApiUrl, Transaction, SystemProgram } from '@solana/web3.js';
+import { Connection, clusterApiUrl, Transaction, SystemProgram, PublicKey, LAMPORTS_PER_SOL } from '@solana/web3.js';
 
 // Ensure Buffer is available globally in the browser environment
 window.Buffer = Buffer;
 // Reactive variables to hold wallet state and address
 const walletConnected = ref(false);
 const walletAddress = ref(null);
+const tokens = ref([]);
+const customRpcUrl = 'https://testnet.dev2.eclipsenetwork.xyz';
+const connection = new Connection(customRpcUrl);
+const balance = ref(0);
+var publicKey = ref(null);
 
 // Function to connect the wallet
 const connectWallet = async () => {
@@ -44,13 +49,11 @@ const disconnectWallet = async () => {
 // Function to send a transaction
 const sendTransaction = async () => {
   if (window.backpack && walletConnected.value) {
-    const customRpcUrl = 'https://testnet.dev2.eclipsenetwork.xyz';
-    const connection = new Connection(customRpcUrl);
-    const { publicKey } = window.backpack;
-
+    publicKey = walletAddress.value;
+    const publicKeyBase58 = new PublicKey(publicKey);
     const transaction = new Transaction().add(
       SystemProgram.transfer({
-        fromPubkey: publicKey,
+        fromPubkey: publicKeyBase58,
         toPubkey: 'DXra5MvE7yY8vmSv2dKMgNw5dvwf6mPJS9B3y1yTfMSD', // Replace with the recipient's public key
         lamports: 1, // 1 SOL = 1,000,000 lamports
       })
@@ -66,9 +69,25 @@ const sendTransaction = async () => {
   }
 };
 
+const getBalance = async () => {
+  if (window.backpack && walletConnected.value) {
+    try {
+      publicKey = walletAddress.value
+      // Assuming you have a PublicKey instance
+      const publicKeyBase58 = new PublicKey(publicKey);
+
+      const lamports = await connection.getBalance(publicKeyBase58);
+      balance.value = lamports / LAMPORTS_PER_SOL;
+    } catch (error) {
+      console.error('Error getting balance:', error);
+    }
+  }
+};
+
 
 const callContractFunction = async () => {
   if (window.backpack && walletConnected.value) {
+    
     const programId = new PublicKey('YourProgramIdHere'); // Replace with your program's public key
     const fromPublicKey = new PublicKey(walletAddress.value);
 
@@ -106,6 +125,26 @@ const callContractFunction = async () => {
   }
 };
 
+const getTokens = async () => {
+  if (walletConnected.value) {
+    try {
+      const publicKey = new PublicKey(walletAddress.value);
+      const tokenAccounts = await connection.getParsedTokenAccountsByOwner(publicKey, {
+        programId: new PublicKey('TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA')
+      });
+
+      tokens.value = tokenAccounts.value.map(tokenAccountInfo => {
+        const accountData = tokenAccountInfo.account.data.parsed.info;
+        return {
+          mint: accountData.mint,
+          amount: accountData.tokenAmount.uiAmountString,
+        };
+      });
+    } catch (error) {
+      console.error('Failed to fetch tokens:', error);
+    }
+  }
+};
 
 </script>
 
@@ -122,6 +161,14 @@ const callContractFunction = async () => {
         </div>
         <button class="btn btn-warning" @click="disconnectWallet" v-if="walletConnected">Disconnect</button>
         <p v-if="walletConnected">Connected: {{ walletAddress }}</p>
+        <button @click="getTokens" v-if="walletConnected">Get My Tokens</button>
+
+        <ul v-if="tokens.length > 0">
+          <li v-for="(token, index) in tokens" :key="index">
+            {{ token.mint }}: {{ token.amount }}
+          </li>
+        </ul>
+        <h6>{{ balance }}</h6>
       </div>
       <!-- Ensure WalletProvider is wrapping the entire section that uses wallet functionality -->
       <WalletProvider :wallets="wallets">
@@ -130,7 +177,7 @@ const callContractFunction = async () => {
           <div class="col-md-4 text-center mb-4" @click="sendTransaction">
             <button class="btn btn-primary">Music</button>
           </div>
-          <div class="col-md-4 text-center mb-4">
+          <div class="col-md-4 text-center mb-4" @click="getBalance">
             <button class="btn btn-primary">Water</button>
           </div>
           <div class="col-md-4 text-center mb-4">
--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,22 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  optimizeDeps: { // 👈 optimizedeps
+    esbuildOptions: {
+      target: "esnext",
+      // Node.js global to browser globalThis
+      define: {
+        global: 'globalThis'
+      },
+      supported: {
+        bigint: true
+      },
+    }
+  },
+
+  build: {
+    target: ["esnext"], // 👈 build.target
+  },
   plugins: [vue()],
   resolve: {
     alias: {
